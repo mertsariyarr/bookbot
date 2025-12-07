bc13017f44af46cf2724f12c07927a7662f27c9b
mertsariyar <mertsariyar2@gmail.com>
2025-12-07 16:36:26 +0300

    added sys module to make our project less 'hard coded' and finish.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from stats import word_counter, char_counter,new_sorted
+import sys
 
+if len(sys.argv) != 2:
+      print("Usage: python3 main.py <path_to_book>")
+      sys.exit(1)
+      
+path = sys.argv[1]
 def main():
-      path = "books/frankenstein.txt"
       text = get_book_text(path)
       num_words = word_counter(text)
       my_chars = char_counter(text)
