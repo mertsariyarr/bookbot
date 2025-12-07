8806a8e29a4360541bc8f9a64d37e681b680b95f
mertsariyar <mertsariyar2@gmail.com>
2025-12-07 14:08:04 +0300

    added functionality to main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("greetings boots")
+def main():
+      path = "books/frankenstein.txt"
+      text = get_book_text(path)
+      num_words = word_counter(text)
+      print(f"Found {num_words} total words")
+
+
+def get_book_text(path):
+        with open(path) as f:
+            book_output = f.read()
+        return book_output
+
+    
+
+def word_counter(book):
+     return len(book.split())
+
+
+
+main()
