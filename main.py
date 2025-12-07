db023f6173fa25f48ae9d5070bd9ad95d9ae9e8e
mertsariyar <mertsariyar2@gmail.com>
2025-12-07 16:28:44 +0300

    added new sorted function to stats.py and clean output

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from stats import word_counter, char_counter
+from stats import word_counter, char_counter,new_sorted
 
 def main():
       path = "books/frankenstein.txt"
       text = get_book_text(path)
       num_words = word_counter(text)
       my_chars = char_counter(text)
-      print(my_chars)
-
+      print("============ BOOKBOT ============")
+      print(f"Analzying book found at {path}...")
+      print("----------- Word Count ----------")
+      print(f"Found {num_words} total words")
+      print("--------- Character Count -------")
+      new_sorted(my_chars)
+      
+    
 
 def get_book_text(path):
         with open(path) as f:
--- stats.py
+++ stats.py
@@ -4,8 +4,23 @@ def word_counter(book):
 def char_counter(book):
      myDict = {}
      for char in book:
-          if char.lower() in myDict:
-               myDict[char.lower()] += 1
-          else:
-               myDict[char.lower()] = 1
+          if char.isalpha():
+            if char.lower() in myDict:
+                myDict[char.lower()] += 1
+            else:
+                myDict[char.lower()] = 1
      return myDict
+
+
+def sort_on(items):
+     return items["num"]
+
+
+def new_sorted(word_dict):
+      myList = []
+      for item in word_dict.keys():
+            myList.append({"char": item, "num": word_dict[item]})               
+      myList.sort(reverse=True, key=sort_on)
+      for item in myList:
+            print(f"{item['char']}: {item['num']}")
+      
